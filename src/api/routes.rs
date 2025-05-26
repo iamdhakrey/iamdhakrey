04568250da7ae96dd38fcbcd4ec4738010009c05
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-26 11:04:16 +0530

    refactor: simplify route handling and update OpenAPI documentation structure

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,34 +1,15 @@
 use axum::Router;
 use utoipa_axum::router::OpenApiRouter;
 
-// use crate::api::v1::auth;
-
 use super::v1::routes::v1_route;
 
-fn prefix_openapi_paths(
-    mut api: utoipa::openapi::OpenApi,
-    prefix: &str,
-) -> utoipa::openapi::OpenApi {
-    let mut new_paths = utoipa::openapi::path::Paths::new();
-    println!("prefix_openapi_paths: {}", prefix);
-    for (path, item) in api.paths.paths.into_iter() {
-        let new_path = format!("{}{}", prefix, path);
-        println!("{} -> {}", path, new_path);
-        new_paths.paths.insert(new_path, item);
-    }
-
-    api.paths.paths = new_paths.paths;
-    api
-}
-
+/// Add routes to the OpenAPI documentation
+/// and return the router and the OpenAPI documentation
 pub fn add_routes(
     api: utoipa::openapi::OpenApi,
 ) -> (Router, utoipa::openapi::OpenApi) {
-    let (router, apis) =
-        OpenApiRouter::with_openapi(api.clone()).split_for_parts();
+    let (_, apis) = OpenApiRouter::<Router>::with_openapi(api.clone())
+        .split_for_parts();
     let (v1_router, api) = v1_route(apis.clone());
-    let router = router.nest("/api", v1_router);
-
-    let api = prefix_openapi_paths(api, "/api");
-    return (router, api);
+    return (v1_router, api);
 }
--- src/api/v1/routes.rs
+++ src/api/v1/routes.rs
@@ -3,11 +3,14 @@ use utoipa_axum::router::OpenApiRouter;
 
 use crate::api::v1::auth;
 
+/// Add routes to the OpenAPI documentation
+/// and return the router and the OpenAPI documentation
+/// Add Prefix '/api/v1' to the OpenAPI documentation
 pub fn v1_route(
     api: utoipa::openapi::OpenApi,
 ) -> (Router, utoipa::openapi::OpenApi) {
-    let (router, api) = OpenApiRouter::with_openapi(api)
-        .nest("/v1", auth::router::auth_router())
+    let (router, api) = OpenApiRouter::with_openapi(api.clone())
+        .nest("/api/v1", auth::router::auth_router())
         .split_for_parts();
     return (router, api);
 }
