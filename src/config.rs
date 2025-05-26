b5dc1fff0b9deabefceda04292af06ae2a347116
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-26 11:04:26 +0530

    refactor: update configuration handling to use Arc<RwLock<AppConfig>>

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,36 @@
+use std::{path::PathBuf, sync::Arc};
+
 use directories::BaseDirs;
 use once_cell::sync::Lazy;
+use std::sync::RwLock;
 
 use crate::models::AppConfig;
 
-pub static CONFIG: Lazy<AppConfig> = Lazy::new(|| load_config());
+// pub static CONFIG: Lazy<AppConfig> = Lazy::new(|| {
+//     let config = load_config();
+//     config
+// });
+
+pub static CONFIG: Lazy<Arc<RwLock<AppConfig>>> = Lazy::new(|| {
+    let config = Arc::new(RwLock::new(load_config()));
+    config
+});
+// pub static CONFIG: Lazy<Arc<RwLock<AppConfig>>> = Lazy::new(|| {
+// let config = load_config();
+// Arc::new(RwLock::new(config))
+// });
+fn config_path() -> PathBuf {
+    let base_dirs = BaseDirs::new().unwrap();
+    let config_dir =
+        base_dirs.config_dir().join("spendlite").join("api.toml");
+    return config_dir;
+}
 
 /// Load the configuration from the config file
 pub fn load_config() -> AppConfig {
     let base_dirs = BaseDirs::new().unwrap();
 
-    let config_dir =
-        base_dirs.config_dir().join("spendlite").join("api.toml");
+    let config_dir = config_path();
 
     // check if not exist then create the config file
     if !config_dir.exists() {
@@ -18,10 +38,10 @@ pub fn load_config() -> AppConfig {
             .unwrap();
         std::fs::write(
             config_dir.clone(),
-            r#"
-url = "sqlite://spendlite.db"
+            r#"database_url = "sqlite://spendlite.db"
 level = "info"
-"#,
+host = "127.0.0.1"
+port = 3000"#,
         )
         .unwrap();
     }
