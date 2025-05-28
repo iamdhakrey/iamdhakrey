540dbc802eef266cf9f0c7f18e14fb78ec2a5519
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-28 18:25:48 +0530

    feat: add SignUpData struct for user registration validation

---
+++ src/api/v1/auth/schema.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+use validator::Validate;
+
+#[derive(Serialize, Deserialize, ToSchema, Validate)]
+pub struct SignUpData {
+    #[validate(length(
+        min = 5,
+        max = 20,
+        message = "Username must be between 5 and 20 characters long"
+    ))]
+    pub username: String,
+
+    #[validate(email(message = "Invalid email address"))]
+    pub email: String,
+    #[validate(length(
+        min = 8,
+        message = "Password must be at least 8 characters long"
+    ))]
+    pub password: String,
+    #[validate(length(
+        min = 2,
+        max = 50,
+        message = "First name must be between 2 and 50 characters long"
+    ))]
+    pub first_name: Option<String>,
+
+    // Optional last name field
+    #[validate(length(
+        min = 2,
+        max = 50,
+        message = "Last name must be between 2 and 50 characters long"
+    ))]
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub last_name: Option<String>,
+}
