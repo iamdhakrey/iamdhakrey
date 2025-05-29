bf5dd3b96dbbfbe8230cc91b0498537a1d27f428
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-29 18:01:28 +0530

    feat: simplify auth router setup and enhance route registration

--- src/api/v1/auth/router.rs
+++ src/api/v1/auth/router.rs
@@ -1,23 +1,14 @@
-use std::sync::Arc;
-
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::{
-    api::v1::auth::handlers as auth, config, db, state::AppState,
-};
-
-use axum::{Router, routing::post};
+use crate::api::v1::auth::handlers as auth;
 
 pub async fn auth_router() -> OpenApiRouter {
-    let router = OpenApiRouter::new()
-        // .route("/signin", post(auth::sign_in))
-        .route("/test", post(auth::test));
+    println!("auth_router called");
+    let router = OpenApiRouter::new().routes(routes!(
+        auth::sign_in,
+        auth::sign_up,
+        auth::test,
+    ));
 
     return router;
 }
-
-// pub fn auth_router() -> Router {
-//     let router = Router::new().route("/signin", post(auth::sign_in));
-//     // .route("/signup", post(auth::sign_up));
-//     return router;
-// }
