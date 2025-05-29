4bbe337d0deb1708a0712d690eeda4040259efdd
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-29 18:01:55 +0530

    refactor: clean up route initialization by removing commented code

--- src/routes.rs
+++ src/routes.rs
@@ -2,15 +2,12 @@ use axum::Router;
 use utoipa_axum::router::OpenApiRouter;
 
 use crate::api::routes;
-use crate::api::v1::auth::handlers::sign_in;
 
 pub async fn add_routes(
     // router: Router,
     api: utoipa::openapi::OpenApi,
 ) -> (Router, utoipa::openapi::OpenApi) {
-    let (router, api) = OpenApiRouter::with_openapi(api)
-        // .route("/api/v1/auth/signin", sign_in)
-        .split_for_parts();
+    let (router, api) = OpenApiRouter::with_openapi(api).split_for_parts();
     let (routes, api) = routes::add_routes(api.clone()).await;
 
     let router = router.merge(routes);
