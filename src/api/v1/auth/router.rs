c68e43e3c06e9f599ada0d237db4a1d43bfd9080
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-30 09:59:02 +0530

    refactor: clean up auth router by removing debug print and simplifying route registration

--- src/api/v1/auth/router.rs
+++ src/api/v1/auth/router.rs
@@ -1,14 +1,11 @@
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::api::v1::auth::handlers as auth;
+use crate::api::v1::auth::handlers::{self as auth};
 
 pub async fn auth_router() -> OpenApiRouter {
-    println!("auth_router called");
-    let router = OpenApiRouter::new().routes(routes!(
-        auth::sign_in,
-        auth::sign_up,
-        auth::test,
-    ));
+    let router = OpenApiRouter::new()
+        .routes(routes!(auth::sign_in))
+        .routes(routes!(auth::sign_up));
 
     return router;
 }
