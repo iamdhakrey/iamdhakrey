819ebdf5ce2012d3021ab2d6680698340c3ea2ea
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-30 09:53:11 +0530

    refactor: streamline sign_in function by removing unnecessary checks and improving error handling

--- src/api/v1/auth/handlers.rs
+++ src/api/v1/auth/handlers.rs
@@ -1,18 +1,11 @@
-use axum::{
-    Error, Extension, Json, debug_handler, extract::State,
-    http::StatusCode, response::IntoResponse,
-};
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use axum::{Extension, Json, http::StatusCode};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use validator::Validate;
 
 use crate::{
-    api::v1::{
-        auth::jwt::encode_jwt,
-        response::{ErrorResponse, GenericResponse},
-    },
-    db,
+    api::v1::{auth::jwt::encode_jwt, response::GenericResponse},
     entities::user,
     state::AppState,
 };
@@ -51,45 +44,38 @@ pub async fn sign_in(
 ) -> Result<Json<String>, GenericResponse<String>> {
     // Check if the email and password are valid
     // let db_state = Extension(AppState);
-    if data.email == "hrithik.d" && data.password == "hrithik@123" {
-        // Create a Claims object with the user's information
+    // Create a Claims object with the user's information
 
-        let _user = user::Entity::find()
-            .filter(user::Column::Email.eq(data.email))
-            .one(&*state.db)
-            .await
-            .map_err(|_| {
-                GenericResponse::<String>::error(
-                    "Database Error".to_string(),
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                )
-            })?
-            .ok_or_else(|| {
-                GenericResponse::<String>::error(
-                    "User not found".to_string(),
-                    StatusCode::NOT_FOUND,
-                )
-            })?;
-        let claims = Claims {
-            sub: "hrithik.d".to_string(),
-            exp: 10000000000,
-            provider: "hrithik".to_string(),
-        };
-        // Return the claims as a JSON response
-        let token = encode_jwt(claims).map_err(|_| {
+    let _user = user::Entity::find()
+        .filter(user::Column::Email.eq(data.email))
+        .one(&*state.db)
+        .await
+        .map_err(|_| {
             GenericResponse::<String>::error(
-                "Failed to Generate Token".to_string(),
+                "Database Error".to_string(),
                 StatusCode::INTERNAL_SERVER_ERROR,
             )
+        })?
+        .ok_or_else(|| {
+            GenericResponse::<String>::error(
+                "{}".to_string(),
+                StatusCode::NOT_FOUND,
+            )
         })?;
-        return Ok(Json(token));
-    } else {
-        // If the email and password are invalid, return a 401 Unauthorized status
-        return Err(GenericResponse::<String>::error(
-            "Invalid email or password".to_string(),
-            StatusCode::UNAUTHORIZED,
-        ));
-    }
+
+    let claims = Claims {
+        sub: "hrithik.d".to_string(),
+        exp: 10000000000,
+        provider: "hrithik".to_string(),
+    };
+    // Return the claims as a JSON response
+    let token = encode_jwt(claims).map_err(|_| {
+        GenericResponse::<String>::error(
+            "Failed to Generate Token".to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        )
+    })?;
+    return Ok(Json(token));
 }
 
 // Define the Auth struct to represent the authentication information
