c698f88ec47f8d32e732a048d3d009870503643e
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-30 17:55:30 +0530

    feat: implement GenericResponse and ValidationErrorResponse for API responses

--- src/response.rs
+++ src/response.rs
@@ -1,1 +1,103 @@
+// generic api response types
+use axum::{
+    Json,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ApiResponse<T>
+where
+    T: Serialize,
+{
+    pub status: String,
+    pub message: Option<String>,
+    pub data: Option<T>,
+}
+pub struct GenericResponse<T>
+where
+    T: Serialize,
+{
+    pub inner: ApiResponse<T>,
+    pub status_code: StatusCode,
+}
+
+impl<T> GenericResponse<T>
+where
+    T: Serialize,
+{
+    pub fn success(
+        status_code: StatusCode,
+        message: Option<String>,
+        data: T,
+        status: String,
+    ) -> Self {
+        Self {
+            inner: ApiResponse {
+                status: status,
+                data: Some(data),
+                message: message,
+            },
+            status_code: status_code,
+        }
+    }
+    pub fn error(message: String, status_code: StatusCode) -> Self {
+        Self {
+            inner: ApiResponse {
+                status: "error".to_string(),
+                message: Some(message),
+                data: None,
+            },
+            status_code,
+        }
+    }
+}
+impl<T> IntoResponse for GenericResponse<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> Response {
+        (self.status_code, Json(self.inner)).into_response()
+    }
+}
+
+pub struct ValidationErrorResponse {
+    pub status: String,
+    pub errors: Vec<String>,
+}
+impl IntoResponse for ValidationErrorResponse {
+    fn into_response(self) -> Response {
+        let response: ApiResponse<Vec<String>> = ApiResponse {
+            status: self.status,
+            message: Some("Validation Failed".to_string()),
+            data: Some(self.errors),
+        };
+        let status_code = StatusCode::UNPROCESSABLE_ENTITY;
+        (status_code, Json(response)).into_response()
+    }
+}
+
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    pub status: String,
+    pub message: String,
+    pub resolution: Option<String>,
+}
+pub struct GenericErrorResponse {
+    pub status: String,
+    pub status_code: StatusCode,
+    pub message: String,
+    pub resolution: Option<String>,
+}
+impl IntoResponse for GenericErrorResponse {
+    fn into_response(self) -> Response {
+        let response = ErrorResponse {
+            status: self.status,
+            message: self.message,
+            resolution: self.resolution,
+        };
+        (self.status_code, Json(response)).into_response()
+    }
+}
