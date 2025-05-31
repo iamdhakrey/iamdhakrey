7d8fc05c5ea11df3e96498100e90cf158e3abc1c
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-31 18:06:23 +0530

    feat: refactor get_user_info function to use UserCheck struct
    - Updated the get_user_info function to accept a UserCheck struct instead of individual parameters.
    - Enhanced error handling by returning StatusCode instead of GenericErrorResponse.
    - Improved user retrieval logic by checking both ID and email.

--- src/api/v1/auth/user/info.rs
+++ src/api/v1/auth/user/info.rs
@@ -1,4 +1,8 @@
+use std::os::linux::raw::stat;
+
+use axum::http::StatusCode;
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+use uuid::Uuid;
 
 use crate::{
     entities::{
@@ -7,18 +11,45 @@ use crate::{
     response::GenericErrorResponse,
 };
 
+#[derive(Debug, Clone)]
+pub struct UserCheck {
+    pub id: Option<Uuid>,
+    pub username: String,
+    pub email: String,
+}
+
 pub async fn get_user_info(
-    username: String,
+    user: UserCheck,
     db: &DatabaseConnection,
-) -> Result<UserModel, GenericErrorResponse> {
+) -> Result<UserModel, StatusCode> {
     // Fetch the user from the database
     match UserEntity::find()
-        .filter(<entities::user::Entity as sea_orm::EntityTrait>::Column::Username.eq(username))
+        .filter(<entities::user::Entity as sea_orm::EntityTrait>::Column::Username.eq(user.username))
         .one(db)
         .await
     {
-        Ok(Some(user)) => Ok(user),
-        Ok(None) => Err(GenericErrorResponse::not_found("User not found")),
-        Err(e) => Err(GenericErrorResponse::internal_error(e.to_string())),
+        Ok(Some(user)) => {
+            let _user_by_id = UserEntity::find()
+                .filter(<entities::user::Entity as sea_orm::EntityTrait>::Column::Id.eq(Some(user.id)))
+                .one(db)
+                .await
+                .map_err(|e| StatusCode::INTERNAL_SERVER_ERROR)?;
+            if _user_by_id.is_some() {
+                let _user_by_email = UserEntity::find()
+                    .filter(<entities::user::Entity as sea_orm::EntityTrait>::Column::Email.eq(user.email.clone()))
+                    .one(db)
+                    .await
+                    .map_err(|e| GenericErrorResponse::internal_error(e.to_string()));
+                if _user_by_email.is_ok() {
+                    Ok(user)
+                } else {
+                    Err(StatusCode::NOT_FOUND)
+                }
+            } else {
+                return Err(StatusCode::NOT_FOUND);
+            }
+        },
+        Ok(None) => Err(StatusCode::NOT_FOUND),
+        Err(e) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
