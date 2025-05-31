dfca610110024a3885093cbb86002e79db0b1adb
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-05-31 17:38:22 +0530

    feat: implement user existence check in create_user function
    - Updated the create_user function to check if a user already exists before proceeding with user creation.
    - Changed the parameter type from CreateUser to SignUpData and updated the return type to GenericErrorResponse.

--- src/api/v1/auth/user/create.rs
+++ src/api/v1/auth/user/create.rs
@@ -1,7 +1,21 @@
+use super::info::get_user_info;
+use crate::{
+    api::v1::auth::schema::SignUpData, entities::prelude::User,
+    entities::user::Entity, response::GenericErrorResponse,
+};
+use sea_orm::DatabaseConnection;
 pub async fn create_user(
-    user: CreateUser,
+    user: SignUpData,
     db: &DatabaseConnection,
-) -> Result<User, ApiError> {
-    let user = User::insert(user).exec(db).await?;
-    Ok(user)
+) -> Result<User, GenericErrorResponse> {
+    // Check if the user already exists
+    let username = user.username.clone();
+    let existing_user = get_user_info(username, db).await;
+    if existing_user.is_ok() {
+        return Err(GenericErrorResponse::conflict(
+            "User already exists".to_string(),
+        ));
+    }
+
+    todo!("Implement user creation logic");
 }
