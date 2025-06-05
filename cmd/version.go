6121c22f9c172d219484fbf2053a03cb4d95a6a6
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-06-05 12:20:26 +0530

    feat: add version command with application details
    - Introduced a new command to print the version number, build time, commit hash, and branch name of the application.
    - Utilizes the cobra library for command-line interface functionality.

--- cmd/version.go
+++ cmd/version.go
@@ -1,1 +1,38 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// Version is the version of the application.
+	Version = "1.0.0"
+
+	// BuildTime is the time when the application was built.
+	BuildTime = "unknown"
+
+	// Commit is the commit hash of the application.
+	// This should be set using -ldflags during the build process.
+	// For example: go build -ldflags "-X 'spendliteapi/cmd.Commit=$(git rev-parse HEAD)'" ./cmd/
+	// This will set the Commit variable to the current git commit hash.
+	Commit = "unknown"
+
+	// Branch is the branch name of the application.
+	// This should be set using -ldflags during the build process.
+	// For example: go build -ldflags "-X 'spendliteapi/cmd.Branch=$(git rev-parse --abbrev-ref HEAD)'" ./cmd/
+	// This will set the Branch variable to the current git branch name.
+	Branch = "unknown"
+)
+var VersionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of storage",
+	Long:  `All software has versions. This is storage's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Storage Version: %s\n", Version)
+		fmt.Printf("Build Time: %s\n", BuildTime)
+		fmt.Printf("Commit: %s\n", Commit)
+		fmt.Printf("Branch: %s\n", Branch)
+	},
+}
