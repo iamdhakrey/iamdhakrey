36f6d7d190181c54f35e30484f7c12b261581203
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2025-06-05 17:34:27 +0530

    feat: enhance server configuration by initializing logger and database
    - Added logger initialization to the Configure function.
    - Initialized the database and set up routes within the Configure function.

--- server/setup.go
+++ server/setup.go
@@ -3,11 +3,18 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"spendlite-api/database"
+	"spendlite-api/logger"
+	"spendlite-api/routes"
 )
 
 func Configure() *echo.Echo {
 	e := echo.New()
+	logger.InitLogger(e)
 	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Recover())
+	db := database.InitDB()
+	routes.SetupRoutes(e, db)
 	return e
 }
